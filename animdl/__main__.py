ead75d1c72624ab632d5c3a0a92fbbec578facf0
justfoolingaround <79979949+justfoolingaround@users.noreply.github.com>
2021-06-28 13:00:13 +0545

    Bump: Version `1.2.2` -> `1.3.0`

--- animdl/__main__.py
+++ animdl/__main__.py
@@ -2,28 +2,27 @@
 A one cli for all the anime.
 """
 
-import shutil
-
 import click
 
-from .core.cli.commands import download, stream, continuation, grab, schedule
-from .core.config import MPV_EXECUTABLE
+from .core import package_banner
+from .core.cli.commands import download, stream, continuation, grab, schedule, test
+from .core.cli.helpers.player import supported_streamers
+
+print("\x1b[35m{}\x1b[39m".format(package_banner))
 
 commands = {
     'download': download.animdl_download,
     'continue': continuation.animdl_continue,
     'grab': grab.animdl_grab,
-    'schedule': schedule.animdl_schedule
+    'schedule': schedule.animdl_schedule,
+    'test': test.animdl_test
 }
 
-mpv = bool(shutil.which(MPV_EXECUTABLE))
+executable = [*supported_streamers()]
 
-if mpv:
+if executable:
     commands.update({'stream': stream.animdl_stream})
-
+	
 @click.group(commands=commands)
 def __animdl_cli__():
     pass
-
-if __name__  == '__main__':
-    __animdl_cli__()
---
+++ animdl/core/__version__.py
@@ -0,0 +1,2 @@
+__core__ = "1.3.0"
+__git_version__ = "c72bfde"
